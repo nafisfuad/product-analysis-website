9a3946770976c4ad2a263a708141aa3fb3944cab
Md. Nafis Fuad <nafisfuad@protonmail.com>
2022-05-31 21:37:06 +0600

    router implemented

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{display: 'flex'}}>
+            <div>
+                <h1>NEXT PRODUCT</h1>
+                <h1>BEST PRODUCT</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati deleniti cumque maxime, nihil dolore facere harum alias accusantium repudiandae, cupiditate laudantium dolorum ducimus amet. Libero ea repellendus odio quis laboriosam.</p>
+            </div>
+            <img src="" alt="" />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>What our customers say!</h1>
+        </div>
+    );
+};
+
+export default Reviews;
