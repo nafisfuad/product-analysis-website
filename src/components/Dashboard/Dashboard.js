1ca980260edd1e123d5349b1a590ee3833387e60
Md. Nafis Fuad <nafisfuad@protonmail.com>
2022-05-31 23:26:17 +0600

    home reviews

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Bar, BarChart, CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import useData from '../../hooks/useData';
 
 const Dashboard = () => {
+    const [data, setData] = useData();
     return (
-        <div>
-            
-        </div>
+        <ResponsiveContainer width="100%" height="100%">
+        <BarChart width={150} height={40} data={data}>
+          <Bar dataKey="uv" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
 
 const Home = () => {
+    const [reviews, setReviews] = useReviews();
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate('/reviews');
+    }
     return (
-        <div style={{display: 'flex'}}>
+        <div>
+            <section>
             <div>
                 <h1>NEXT PRODUCT</h1>
                 <h1>BEST PRODUCT</h1>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati deleniti cumque maxime, nihil dolore facere harum alias accusantium repudiandae, cupiditate laudantium dolorum ducimus amet. Libero ea repellendus odio quis laboriosam.</p>
             </div>
-            <img src="" alt="" />
+            <img src="../../images/Products-And-Services.jpg" alt="" />
+            </section>
+            <section>
+                <div className='reviews'>
+                    {
+                        reviews.slice(0, 3).map(review => <Review key={review.id} review={review}></Review>)
+                    }
+                    
+                </div>
+            
+            </section>
+            <button onClick={handleNavigate}>See All Reviews</button>
         </div>
     );
 };
